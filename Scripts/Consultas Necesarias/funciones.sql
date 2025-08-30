2d28bf3cc613451d24710d4033a2e94bf9b337dc
daviddev <davidtejadamoreta26@gmail.com>
2025-08-30 12:31:04 -0400

    feat: Se agregaron nuevas funciones

--- Scripts/Consultas Necesarias/funciones.sql
+++ Scripts/Consultas Necesarias/funciones.sql
@@ -62,12 +62,17 @@ $$ LANGUAGE plpgsql;
 
 --provincias mas cercana a otra
 
-CREATE OR REPLACE FUNCTION provincias_mas_cercanas(provincia_origen TEXT, limite INT)
-RETURNS TABLE (
-  codigo_provincia TEXT,
-  nombre_provincia TEXT,
-  distancia_metros DOUBLE PRECISION
-) AS $$
+CREATE FUNCTION provincias_mas_cercanas(
+    provincia_origen TEXT,
+    limite INTEGER
+)
+RETURNS TABLE(
+    codigo_provincia TEXT,
+    nombre_provincia TEXT,
+    distancia_metros DOUBLE PRECISION,
+    ubicacion geometry
+)
+AS $$
 BEGIN
   RETURN QUERY
   SELECT 
@@ -76,7 +81,8 @@ BEGIN
     ST_Distance(
       ST_Centroid(ST_Transform(p1.wkb_geometry, 4326))::geography,
       ST_Centroid(ST_Transform(p2.wkb_geometry, 4326))::geography
-    ) AS distancia_metros
+    ) AS distancia_metros,
+    p2.wkb_geometry AS ubicacion
   FROM provincias p1
   JOIN provincias p2 ON p1.prov <> p2.prov
   WHERE p1.prov = provincia_origen
@@ -85,6 +91,69 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--distancia cercanos
+
+CREATE OR REPLACE FUNCTION distancia_y_cercanos(
+    tabla_origen TEXT,
+    tipo_origen TEXT,
+    codigo_origen TEXT,
+    tabla_destino TEXT,
+    tipo_destino TEXT,
+    limite INTEGER DEFAULT 5
+)
+RETURNS TABLE(
+    codigo TEXT,
+    nombre TEXT,
+    distancia_metros DOUBLE PRECISION,
+    ubicacion_geojson JSON
+)
+AS $$
+DECLARE
+    col_origen TEXT;
+    col_destino TEXT;
+BEGIN
+    -- Mapear tipo a columna de código
+    col_origen := CASE tipo_origen
+                    WHEN 'prov' THEN 'prov'
+                    WHEN 'mun'  THEN 'mun'
+                    WHEN 'sec'  THEN 'sec'
+                    WHEN 'dm'   THEN 'dm'
+                    WHEN 'dp'   THEN 'dp'
+                  END;
+
+    col_destino := CASE tipo_destino
+                    WHEN 'prov' THEN 'prov'
+                    WHEN 'mun'  THEN 'mun'
+                    WHEN 'sec'  THEN 'sec'
+                    WHEN 'dm'   THEN 'dm'
+                    WHEN 'dp'   THEN 'dp'
+                  END;
+
+    RETURN QUERY EXECUTE format(
+        'SELECT 
+            d.%I::TEXT AS codigo,
+            d.toponimia::TEXT AS nombre,
+            ST_Distance(
+                ST_Centroid(ST_Transform(o.wkb_geometry, 4326))::geography,
+                ST_Centroid(ST_Transform(d.wkb_geometry, 4326))::geography
+            ) AS distancia_metros,
+            ST_AsGeoJSON(d.wkb_geometry)::JSON AS ubicacion_geojson
+         FROM %I o
+         JOIN %I d ON o.%I <> d.%I
+         WHERE o.%I = $1
+         ORDER BY distancia_metros
+         LIMIT $2',
+         col_destino,
+         tabla_origen,
+         tabla_destino,
+         col_origen,
+         col_destino,
+         col_origen
+    )
+    USING codigo_origen, limite;
+
+END;
+$$ LANGUAGE plpgsql;
 
 -- -- Ejemplo: las 4 provincias más cercanas a la provincia '21'
 -- SELECT * FROM provincias_mas_cercanas('21', 4);
